83a8791dc0fad4d6e9311e934074465a15f22a24
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2025-01-12 11:24:14 -0300

    Corrige fetchPageInfo e configurações de pré-renderização

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,13 +1,11 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
-
-import { dataset, projectId } from '../env'
+import createImageUrlBuilder from '@sanity/image-url';
+import { dataset, projectId } from '../env';
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
   dataset: dataset || '',
-})
+});
 
-export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto('format').fit('max').url()
-}
+export const urlForImage = (source: any) => {
+  return imageBuilder.image(source).auto('format').fit('max').url();
+};
