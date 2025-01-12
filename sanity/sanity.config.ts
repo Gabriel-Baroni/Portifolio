d7f20d525ca4a600d2d277e1505d33654ebc96b4
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2025-01-12 11:46:44 -0300

    Corrige fetchPageInfo e configurações de pré-renderização

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,10 @@
+/**
+ * This configuration is used for the Sanity Studio that’s mounted on the /pages/studio/[[...index]].tsx route
+ */
+
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
+import { structureTool } from 'sanity/structure';
 
 // Vá para https://www.sanity.io/docs/api-versioning para aprender como funciona o versionamento de API
 import { apiVersion, dataset, projectId } from './env';
@@ -13,7 +17,7 @@ export default defineConfig({
   // Adicione e edite o esquema de conteúdo na pasta './sanity/schema'
   schema,
   plugins: [
-    deskTool(),
+    structureTool(),
     // Vision é uma ferramenta que permite consultar seu conteúdo com GROQ no estúdio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
