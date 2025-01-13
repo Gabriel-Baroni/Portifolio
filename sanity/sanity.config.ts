898cfafb1864b450b20fe6d66a1dd0fd941c01f4
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2025-01-13 17:20:29 -0300

    tirando o defineConfig que é do v3

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -3,23 +3,17 @@
  */
 
 import { visionTool } from '@sanity/vision';
-import { defineConfig } from 'sanity'; // Correct import for Studio configuration
-import { structureTool } from 'sanity/structure';
-
-// Vá para https://www.sanity.io/docs/api-versioning para aprender como funciona o versionamento de API
-import { apiVersion, dataset, projectId } from './env';
+// No Sanity v2, não existe `defineConfig`
 import { schema } from './schema';
+import { apiVersion, dataset, projectId } from './env';
 
-export default defineConfig({
+export default {
   basePath: '/studio',
   projectId,
   dataset,
-  // Adicione e edite o esquema de conteúdo na pasta './sanity/schema'
   schema,
   plugins: [
-    structureTool(),
-    // Vision é uma ferramenta que permite consultar seu conteúdo com GROQ no estúdio
-    // https://www.sanity.io/docs/the-vision-plugin
+    // Vision permite consultas com GROQ
     visionTool({ defaultApiVersion: apiVersion }),
   ],
-});
+};
